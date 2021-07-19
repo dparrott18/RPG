d3cddd698e5105c9fb3f57b044cd66c854f1430e
dparrott18 <86675097+dparrott18@users.noreply.github.com>
2021-07-18 20:46:07 -0400

    Global Files fix

--- ScriptFiles/Money.ps1
+++ ScriptFiles/Money.ps1
@@ -2,40 +2,34 @@
 {
     [cmdletbinding()]
     param()
-
-    
-
-    Write-Output "Choose the Index of the campaign`n1: Partharia (Elric)`n2: Roman Ragnarok (Marcus Flaminius Cincinatus)`n3: Partheria (Gaelei Mahannen)"
+ 
+    write-host "Choose the Index of the campaign`n1: Partharia (Elric)`n2: Roman Ragnarok (Marcus Flaminius Cincinatus)`n3: Partheria (Gaelei Mahannen)"
     [int]$Index = Read-Host "Index?"
-
+    $CampaignRoot = "$env:OneDriveConsumer\RPGs\Campaigns"
     switch ($index)
-    {
-        1
         {
-            $Files = New-Object -TypeName psobject 
-            $Files | add-member -Name "PC" -Value "$env:OneDriveConsumer\RPGs\Campaigns\Partheria\Money\PC.txt" -MemberType NoteProperty
-            $Files | add-member -Name "PCBank" -value "$env:OneDriveConsumer\RPGs\Campaigns\Partheria\Money\PCBank.txt" -MemberType NoteProperty
-            $Files | add-member -Name "PartyFund" -value "$env:OneDriveConsumer\RPGs\Campaigns\Partheria\Money\PartyFund.txt" -MemberType NoteProperty
-            $Files | add-member -Name "Tax" -Value "$env:OneDriveConsumer\RPGs\Campaigns\Partheria\Money\Tax.txt" -MemberType NoteProperty
+            1
+            { 
+                $Files | add-member -Name "PC" -Value "$CampaignRoot\Partheria\Money\PC.txt" -MemberType NoteProperty
+                $Files | add-member -Name "PCBank" -value "$CampaignRoot\Partheria\Money\PCBank.txt" -MemberType NoteProperty
+                $Files | add-member -Name "PartyFund" -value "$CampaignRoot\Partheria\Money\PartyFund.txt" -MemberType NoteProperty
+                $Files | add-member -Name "Tax" -Value "$CampaignRoot\Partheria\Money\Tax.txt" -MemberType NoteProperty
+            }
+            2
+            {
+                $Files | add-member -Name "PC" -Value "$CampaignRoot\Roman Ragnarok\Money\PC.txt" -MemberType NoteProperty
+                $Files | add-member -Name "PCBank" -value "$CampaignRoot\Roman Ragnarok\Money\PCBank.txt" -MemberType NoteProperty
+                $Files | add-member -Name "PartyFund" -value "$CampaignRoot\Roman Ragnarok\Money\PartyFund.txt" -MemberType NoteProperty
+                $Files | add-member -Name "Tax" -Value "$CampaignRoot\Roman Ragnarok\Money\Tax.txt" -MemberType NoteProperty
+            }
+            3
+            { 
+                $Files | add-member -Name "PC" -Value "$CampaignRoot\Partheria2\Money\PC.txt" -MemberType NoteProperty
+                $Files | add-member -Name "PCBank" -value "$CampaignRoot\Partheria2\Money\PCBank.txt" -MemberType NoteProperty
+                $Files | add-member -Name "PartyFund" -value "$CampaignRoot\Partheria2\Money\PartyFund.txt" -MemberType NoteProperty
+                $Files | add-member -Name "Tax" -Value "$CampaignRoot\Partheria2\Money\Tax.txt" -MemberType NoteProperty
+            }
         }
-        2
-        {
-            $Files = New-Object -TypeName psobject 
-            $Files | add-member -Name "PC" -Value "E:\OneDrive\OneDrive\RPGs\Campaigns\Roman Ragnarok\Money\PC.txt" -MemberType NoteProperty
-            $Files | add-member -Name "PCBank" -value "E:\OneDrive\OneDrive\RPGs\Campaigns\Roman Ragnarok\Money\PCBank.txt" -MemberType NoteProperty
-            $Files | add-member -Name "PartyFund" -value "E:\OneDrive\OneDrive\RPGs\Campaigns\Roman Ragnarok\Money\PartyFund.txt" -MemberType NoteProperty
-            $Files | add-member -Name "Tax" -Value "E:\OneDrive\OneDrive\RPGs\Campaigns\Roman Ragnarok\Money\Tax.txt" -MemberType NoteProperty
-        }
-        3
-        {
-            $Files = New-Object -TypeName psobject 
-            $Files | add-member -Name "PC" -Value "E:\OneDrive\OneDrive\RPGs\Campaigns\Partheria2\Money\PC.txt" -MemberType NoteProperty
-            $Files | add-member -Name "PCBank" -value "E:\OneDrive\OneDrive\RPGs\Campaigns\Partheria2\Money\PCBank.txt" -MemberType NoteProperty
-            $Files | add-member -Name "PartyFund" -value "E:\OneDrive\OneDrive\RPGs\Campaigns\Partheria2\Money\PartyFund.txt" -MemberType NoteProperty
-            $Files | add-member -Name "Tax" -Value "E:\OneDrive\OneDrive\RPGs\Campaigns\Partheria2\Money\Tax.txt" -MemberType NoteProperty
-        }
-    }
-
     $Files
 }
 
